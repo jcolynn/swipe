3c2b379bec01a5d270aec2dafe5fd06fe0a13efd
Cody Lynn <jclynn87@gmail.com>
2018-05-11 19:51:58 -0400

    Setup Deck for renderCard

--- App.js
+++ App.js
@@ -18,11 +18,20 @@ const DATA = [
 
 
 export default class App extends React.Component {
+  renderCard(item) {
+    return (
+      <Text>
+        {item.text}
+      </Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Deck
-          
+          data={DATA}
+          renderCard={this.renderCard}
         />
       </View>
     );
